5fa4c42e232c1bde606b0c3b323f96383983137d
Ivan Koposhilov <ikoposhilov@yandex.ru>
2022-04-14 23:53:36 +0700

    refactoring some functions, git, readme, add decorators

--- quest_cert_parser/parser_with_gui.py
+++ quest_cert_parser/parser_with_gui.py
@@ -1,6 +1,6 @@
 __author__ = "Ivan Koposhilov"
 __copyright__ = "Copyright (C) 2021 Ivan Koposhilov"
-__version__ = "1.0"
+__version__ = "2.0"
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,36 +17,61 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 big_data = []
 
-def add_user():
+def save_certification(func):
+    def wrapper():
+        result = func()
+        with open ('certifications.txt', 'w+') as f:
+            f.writelines("\n".join(cert_listbox.get(0, 'end')))
+        return result
+    return wrapper
+
+def select_listbox(func):
+    def wrapper():
+        if rs_listbox.curselection():
+            lstbx = rs_listbox
+        else:
+            lstbx = slb_listbox
+        result = func(lstbx)
+        return result
+    return wrapper
+
+def save_users(func):
+    def wrapper():
+        result = func()
+        if result == slb_listbox:
+            file = 'slb_user.txt'
+        else:
+            file = 'rs_user.txt'
+        with open (file, 'w+') as f:
+            f.writelines("\n".join(result.get(0, 'end')))
+        return result
+    return wrapper  
+
+@save_users
+@select_listbox
+def add_user(lstbx):
     data_entry = f'{gin_entry.get()}: {name_entry.get()}'
-    if slb_listbox.curselection():
-        lsbx = slb_listbox
-        slb_listbox.insert('end', data_entry)
-    else:
-        lsbx = rs_listbox
-        rs_listbox.insert('end', data_entry)
+    lstbx.insert('end', data_entry)
     name_entry.delete(0, 'end')
     gin_entry.delete(0, 'end')
-    save_user(lsbx)
-    
-def del_user():
-    if slb_listbox.curselection():
-        lsbx = slb_listbox
-        select_user = slb_listbox.curselection()
-        slb_listbox.delete(select_user)
-    else:
-        lsbx = rs_listbox
-        select_user = rs_listbox.curselection()
-        rs_listbox.delete(select_user)
-    save_user(lsbx)
+    return lstbx
 
-def save_user(lsbx):
-    if lsbx == slb_listbox:
-        with open ('slb_user.txt', 'w+') as f:
-            f.writelines("\n".join(lsbx.get(0, 'end')))
-    else:
-        with open ('rs_user.txt', 'w+') as f:
-            f.writelines("\n".join(lsbx.get(0, 'end')))
+@save_users
+@select_listbox 
+def del_user(lstbx):
+    select_user = lstbx.curselection()
+    lstbx.delete(select_user)
+    return lstbx
+    
+@save_certification    
+def add_certification():
+    certification = f'{cert_entry.get()}' 
+    cert_listbox.insert('end', certification)
+
+@save_certification 
+def del_certification():
+    select_certification = cert_listbox.curselection()
+    cert_listbox.delete(select_certification)
 
 def show_result(message):
     text.configure(state='normal')
@@ -64,20 +89,22 @@ def check_thread(thread):
         window.after(100, lambda: check_thread(thread))
     else:
         start_button.config(state=tk.NORMAL)
+        
+def save_excel():
+    pass
 
 def load_data():   
     """
     Функция для загрузки в словарь: GIN / имя и в список: необходимые сертификации
     Returns: dict, list
     """
-    l = r_var.get()
-    if  l == 1:
+    value_rbutton = select_listbox_button.get()
+    if  value_rbutton == 1:
         lstbx = rs_listbox
     else:
         lstbx = slb_listbox
         
     people_dict = {}
-    #people_dict = dict((a, b) for a, b in str(slb_listbox.get(elem).split(': ')) for elem in range(slb_listbox.size()))
     
     for elem in range(lstbx.size()):
         gin_name = str(lstbx.get(elem)).split(': ')
@@ -87,7 +114,7 @@ def load_data():
 
 def connect_quest(people_dict):
     """
-    функция для сбора информаии с Quest
+    функция для сбора информации с Quest
 
     Args:
         certifications (_list_): список необходимых сертифкаций
@@ -98,9 +125,7 @@ def connect_quest(people_dict):
     """
     
     for gin in people_dict.keys():
-        
         name = people_dict[gin]
-        
         url = f'{URL}{gin}'
 
         response = requests.get(url, verify=False)
@@ -171,11 +196,11 @@ def collect_data(tables, name, people_dict):
             
     big_data.append([name, *gin_data])
         
-    format_file(big_data, certifications, people_dict)
+    format_file(big_data, certifications)
 
-def format_file(big_data, certifications, people_dict):
+def format_file(big_data, certifications):
     date_time = datetime.now()
-    date = date_time.strftime('%d-%m-%Y')
+    date = date_time.strftime('%Y-%m-%d')
     wb = openpyxl.Workbook()
     ws = wb.active
     ws.append(['Name/Certifications', *certifications])
@@ -189,7 +214,8 @@ def format_file(big_data, certifications, people_dict):
     style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False, showLastColumn=False, showRowStripes=True, showColumnStripes=False)
     tab.tableStyleInfo = style
     ws.add_table(tab)
-
+    ws.column_dimensions['A'].width = 23 
+    
     for sheet in wb.worksheets:
         for row in sheet:
             for cell in row:
@@ -197,8 +223,8 @@ def format_file(big_data, certifications, people_dict):
                   cell.fill = PatternFill(start_color='ff0000', end_color='ff0000', fill_type='solid')
                 if cell.value in certifications:
                     cell.alignment = Alignment(vertical='center', horizontal='center', text_rotation=90)
-    try:                
-        ws.column_dimensions['A'].width = 23
+    
+    try:                  
         wb.save(f'BOM_{date}.xlsx')
         message = f', данные записаны в файл \n'
     except:
@@ -247,20 +273,20 @@ rs_label.place(x=290, y=100)
 cert_label = tk.Label(text='CERTIFICATIONS')
 cert_label.place(x=650, y=15)
 
-add_cert_button = tk.Button(text="ADD", height = 1, width=7)
+add_cert_button = tk.Button(text="ADD", height = 1, width=7, command=add_certification)
 add_cert_button.place(x=690, y=70)
 
-del_cert_button = tk.Button(text='DELETE')
+del_cert_button = tk.Button(text='DELETE', command=del_certification)
 del_cert_button.place(x=760, y=70)
 
 cert_entry = tk.Entry(width=40)
 cert_entry.place(x=570, y=40)
 
-r_var = tk.IntVar()
-r_var.set(0)
-slb_rbtn = tk.Radiobutton(text='SLB', variable=r_var, value=0)
+select_listbox_button = tk.IntVar()
+select_listbox_button.set(0)
+slb_rbtn = tk.Radiobutton(text='SLB', variable=select_listbox_button, value=0)
 slb_rbtn.place(x=600, y=480)
-rs_rbtn = tk.Radiobutton(text='RS', variable=r_var, value=1)
+rs_rbtn = tk.Radiobutton(text='RS', variable=select_listbox_button, value=1)
 rs_rbtn.place(x=600, y=500)
 
 file_dict = {'slb_user.txt': slb_listbox, 'rs_user.txt': rs_listbox, 'certifications.txt': cert_listbox}
